915e8628ec05398b5b6922322fc209a5823f808e
Hossein A. Rahmani <rahmanidashti@gmail.com>
2025-09-05 11:11:19 +0100

    add EvalData class for reasoning data

---
+++ reasoning_eval/eval_data.py
@@ -0,0 +1,35 @@
+from datasets import load_dataset
+import pandas as pd
+import json
+import argparse
+
+class EvalData:
+
+    def __init__(self, category: str = "Age"):
+        self.category = category
+
+    def load_BBQ_dataset(self, dataset_path: str = "heegyu/bbq") -> dict:
+        dataset = load_dataset(dataset_path, self.category)
+        dataset = dataset['test']
+        print(f"INFO:: Loaded BBQ dataset with {len(dataset)} samples.")
+        return dataset
+    
+    def load_BBQ_metadata(self, metadata_path: str = "../datasets/bbq_additional_metadata.csv"):
+        # load metadata
+        metadata = pd.read_csv(metadata_path)
+        metadata = metadata[metadata['category'] == self.category]
+        print(f"INFO:: Loaded BBQ metadata with {len(metadata)} samples.")
+        return metadata
+    
+    def load_BBQ_templates(self):
+        template_path = f"../datasets/bbq_templates/new_templates - {self.category}.csv"
+        template_df = pd.read_csv(template_path)
+        return template_df
+    
+    def load_reasoning_data(reasoning_data_path: str = "../outputs/qwen_8B_full/bbq_Age_results_merged.json"):
+        with open(reasoning_data_path, "r") as f:
+            data = json.load(f)
+        reasoning_data = data['results']
+        print(f"INFO:: Loaded BBQ reasoning data with {len(reasoning_data)} samples.")
+        return reasoning_data
+
